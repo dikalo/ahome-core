8d0a9226ea59a61e20bd8775ef74d90c1ff1a3d2
eemi2010 <jazzmatadazz@gmail.com>
2014-05-23 00:41:27 +0200

    Utility class to inject jQuery in the application

---
+++ ahome-core/src/com/ait/toolkit/core/client/JQueryResources.java
@@ -0,0 +1,10 @@
+package com.ait.toolkit.core.client;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.TextResource;
+
+public interface JQueryResources extends ClientBundle {
+
+	@Source("jq.js")
+	TextResource jq();
+}
