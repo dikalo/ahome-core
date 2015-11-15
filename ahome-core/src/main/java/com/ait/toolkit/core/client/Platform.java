319c849690c84a430a5139c5432b1a83f6f75bdf
eemi210 <ekambi@ahome-it-com>
2015-11-15 21:21:00 +0100

    move api to 0.13

--- ahome-core/src/main/java/com/ait/toolkit/core/client/Platform.java
+++ ahome-core/src/main/java/com/ait/toolkit/core/client/Platform.java
@@ -1,28 +1,19 @@
 package com.ait.toolkit.core.client;
 
-/**
- * Detects in wich platform that app is running
- */
 public class Platform {
 
-	private Platform() {
+    private Platform() {
 
-	}
+    }
 
-	/**
-	 * Returns true if we are running inside NodeWebkit
-	 */
-	public static native boolean isNodeWebkit()/*-{
-		try {
-			var nw = $wnd.require('nw.gui');
-			if (nw) {
-				return true;
-			}
-			return false;
-		} catch (e) {
-			return false;
+    /**
+    * Returns true if we are running inside NodeWebkit
+    */
+    public static native boolean isNodeWebkit()/*-{
+		if ($wnd.nw && $wnd.nw.process && $wnd.nw.process.versions) {
+			return true;
 		}
-
-	}-*/;
+		return false;
+    }-*/;
 
 }
